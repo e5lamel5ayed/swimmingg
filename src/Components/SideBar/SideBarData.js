40c400a3fe814d41f4b0049d094b6a869909cf66
sayedosama7 <sayedosama088@gmail.com>
2024-05-19 16:15:56 -0700

    nav

--- src/Components/SideBar/SideBarData.js
+++ src/Components/SideBar/SideBarData.js
@@ -2,13 +2,38 @@ import React from 'react'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 export const SidebarData = [
     {
-        title: "Hello,Sign in",
+        title: "مرحبا بك",
         path: "/",
         icon: <AccountCircleIcon />,
         cName: "nav-text"
     },
     {
-        title: "MobilePhones",
+        title: "الرئيسية",
+        path: "/",
+        cName: "nav-text"
+    },
+    {
+        title: "الرئيسية",
+        path: "/",
+        cName: "nav-text"
+    },
+    {
+        title: "الرئيسية",
+        path: "/",
+        cName: "nav-text"
+    },
+    {
+        title: "الرئيسية",
+        path: "/",
+        cName: "nav-text"
+    },
+    {
+        title: "الرئيسية",
+        path: "/",
+        cName: "nav-text"
+    },
+    {
+        title: "الرئيسية",
         path: "/",
         cName: "nav-text"
     },
