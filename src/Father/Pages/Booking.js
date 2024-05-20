3c56ccfe71ac9b22a75181f947635c50b5c05610
sayedosama7 <sayedosama088@gmail.com>
2024-05-20 14:02:06 +0300

    father-emploee

---
+++ src/Father/Pages/Booking.js
@@ -0,0 +1,109 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FiUsers } from "react-icons/fi";
+import { FaSwimmer } from "react-icons/fa";
+import NavBar from '../Components/NavBar/NavBar';
+
+
+const ClassHome = () => {
+    return (
+        <div>
+            <NavBar />
+            <div className="container booking">
+                <div className="row">
+
+                    <div className="col-md-12 text-center text-white my-5">
+                        <h5 className='font-italic'>لنري ما هو متاح</h5>
+                        <h2 className='mt-3'>ماذا تريد ان تري</h2>
+                    </div>
+
+                    <div className="col-md-4 text-white">
+                        <Link to='/classes' className='text-decoration-none'>
+                            <div className='box px-4'>
+                                <h4 className='text-info'>
+                                    بحث عن الصف
+                                </h4>
+                                <div>
+                                    <FiUsers size={60} className='icon' />
+                                </div>
+                            </div>
+                        </Link>
+                    </div>
+
+                    <div className="col-md-4 text-white">
+                        <Link to='/' className='text-decoration-none'>
+                            <div className='box px-4'>
+                                <h4 className='text-info'>
+                                    الصيف مايو <br /> 1-12-2024
+                                </h4>
+                                <div>
+                                    <FaSwimmer size={60} className='icon' />
+                                </div>
+                            </div>
+                        </Link>
+                    </div>
+
+                    <div className="col-md-4 text-white">
+                        <Link to='/' className='text-decoration-none'>
+                            <div className='box px-4'>
+                                <h4 className='text-info'>
+                                    الصيف مايو <br /> 1-12-2024
+                                </h4>
+                                <div>
+                                    <FaSwimmer size={60} className='icon' />
+                                </div>
+                            </div>
+                        </Link>
+                    </div>
+
+                    <div className="col-md-4 text-white">
+                        <Link to='/' className='text-decoration-none'>
+                            <div className='box px-4'>
+                                <h4 className='text-info'>
+                                    الصيف مايو <br /> 1-12-2024
+                                </h4>
+                                <div>
+                                    <FaSwimmer size={60} className='icon' />
+                                </div>
+                            </div>
+                        </Link>
+                    </div>
+
+                    <div className="col-md-4 text-white">
+                        <Link to='/' className='text-decoration-none'>
+                            <div className='box px-4'>
+                                <h4 className='text-info'>
+                                    الصيف مايو <br /> 1-12-2024
+                                </h4>
+                                <div>
+                                    <FaSwimmer size={60} className='icon' />
+                                </div>
+                            </div>
+                        </Link>
+                    </div>
+
+                    <div className="col-md-4 text-white">
+                        <Link to='/' className='text-decoration-none'>
+                            <div className='box px-4'>
+                                <h4 className='text-info'>
+                                    الصيف مايو <br /> 1-12-2024
+                                </h4>
+                                <div>
+                                    <FaSwimmer size={60} className='icon' />
+                                </div>
+                            </div>
+                        </Link>
+                    </div>
+
+
+
+
+                </div>
+
+                {/* </div> */}
+            </div>
+        </div>
+    )
+}
+
+export default ClassHome
