4b4f4925172e59f20b1a2e5e20c15f35325e4062
sayedosama7 <sayedosama088@gmail.com>
2024-05-22 16:57:55 +0300

    account

--- src/Father/Pages/MyAcount/MyAcountFather.js
+++ src/Father/Pages/MyAcount/MyAcountFather.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import TransactionHistory from './TransactionHistory';
 import Payment from './Payment';
+import AccountInfo from './AccountInfo';
 
 
 export const MyAcount = () => {
@@ -51,6 +52,7 @@ export const MyAcount = () => {
 
                             {tabValue === 0 && (
                                 <div>
+                                    <AccountInfo/>
                                     <Student />
 
                                 </div>
--- src/Father/Pages/MyAcount/Payment.js
+++ src/Father/Pages/MyAcount/Payment.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Payment = () => {
     return (
         <div>
-            <div className=" mt-4 bg-white payment">
+            <div className=" mt-4 bg-white all-account">
                 <div className=' pt-4'>
                     <h3
                         style={{
