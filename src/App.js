08aa86fbdba1a5f54f4e0e329b7e0c2ea4cd9494
e5lam el5ayed <eslam25elsayed12@gmail.com>
2024-06-03 14:11:49 +0300

    يعني خلاص نا مش هشوفك تاني

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Home from './Employee/Empolee';
-import Loading from './Loading';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 1800);
-  }, []);
 
   return (
     <div>
-      {loading ? <Loading /> : <Home />}
+      <Home />
     </div>
   );
 }
